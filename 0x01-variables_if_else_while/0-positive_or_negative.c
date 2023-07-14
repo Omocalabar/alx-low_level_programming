f1522ba0bc053404367a6266c0e6ae436473142c
Umoh <umoh2imoh9@gmail.com>
2023-07-14 07:39:34 -0700

    Variables if else while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int n;
+
+    srand(time(0)); // Initialize random seed
+
+    n = rand() % 201 - 100; // Generate a random number between -100 and 100
+
+    printf("The number is %d\n", n);
+
+    if (n > 0) {
+        printf("is positive\n");
+    } else if (n == 0) {
+        printf("is zero\n");
+    } else {
+        printf("is negative\n");
+    }
+
+    printf("\n");
+
+    return 0;
+}
