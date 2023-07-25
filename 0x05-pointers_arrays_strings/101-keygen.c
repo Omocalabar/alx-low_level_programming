b37e3e36aa1e1a68d0dd6521a11fd9ce62e8f93f
Umoh <umoh2imoh9@gmail.com>
2023-07-24 19:15:55 -0700

    Arrays Strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -23,14 +23,12 @@ int main(void)
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
-		if ((2772 - sum) < 78)
-{
-    n = 2772 - sum;
-    sum += n;
-    putchar(n + '0');
-    break;
-}
-
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
 
 	return (0);
